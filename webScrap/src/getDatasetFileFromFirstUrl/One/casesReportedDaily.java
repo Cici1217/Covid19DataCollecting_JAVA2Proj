eedd9f926d667c9f5ac02170b150129fbf905292
Cici1217 <mzcxxczm@126.com>
2021-12-02 01:44:57 +0800

    第一个表格处理完成

---
+++ webScrap/src/getDatasetFileFromFirstUrl/One/casesReportedDaily.java
@@ -0,0 +1,73 @@
+package getDatasetFileFromFirstUrl.One;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class casesReportedDaily {
+    public static void main(String[] args) {
+        File file = new File("casesReportedDaily.csv");
+        HashMap<String, ArrayList<OneData>> hashMap = new HashMap<>();
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String temp;
+            temp = reader.readLine();
+            while ((temp = reader.readLine()) != null) {
+                String[] body = temp.split(",", -1);
+                OneData oneData = new OneData();
+                if (body.length > 8) {
+                    String t = body[2].substring(1) + "," + body[3].substring(0, body[3].length() - 1);
+                    oneData.setDate_reported(body[0]);
+                    oneData.setCountry_code(body[1]);
+                    oneData.setCountry(t);
+                    oneData.setWHO_region(body[4]);
+                    oneData.setNew_cases(body[5]);
+                    oneData.setCumulative_cases(body[6]);
+                    oneData.setNew_deaths(body[7]);
+                    oneData.setCumulative_deaths(body[8]);
+                    if (hashMap.containsKey(t)) {
+                        hashMap.get(t).add(oneData);
+                        hashMap.put(t, hashMap.get(t));
+                    } else {
+                        ArrayList<OneData> a = new ArrayList<>();
+                        a.add(oneData);
+                        hashMap.put(t, a);
+                    }
+                } else {
+                    oneData.setDate_reported(body[0]);
+                    oneData.setCountry_code(body[1]);
+                    oneData.setCountry(body[2]);
+                    oneData.setWHO_region(body[3]);
+                    oneData.setNew_cases(body[4]);
+                    oneData.setCumulative_cases(body[5]);
+                    oneData.setNew_deaths(body[6]);
+                    oneData.setCumulative_deaths(body[7]);
+                    if (hashMap.containsKey(body[2])) {
+                        hashMap.get(body[2]).add(oneData);
+                        hashMap.put(body[2], hashMap.get(body[2]));
+                    } else {
+                        ArrayList<OneData> a = new ArrayList<>();
+                        a.add(oneData);
+                        hashMap.put(body[2], a);
+                    }
+                }
+
+            }
+//            String n = "occupied Palestinian territory, including east Jerusalem";
+//            int x = hashMap.get(n).size();
+//            for (int i = 0; i < x; i++) {
+//                OneData oneData = hashMap.get(n).get(i);
+//                System.out.print(oneData.getNew_cases() + " ");
+//                System.out.println(oneData.getCumulative_cases());
+//            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //You can process hashmap here
+
+
+    }
+}
---
+++ webScrap/src/getDatasetFileFromFirstUrl/getFiles.java
@@ -0,0 +1,62 @@
+package getDatasetFileFromFirstUrl;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class getFiles {
+    public static void main(String[] args) throws Exception {
+        String origin = "https://covid19.who.int";
+        String url = origin + "/table";
+        Document doc = Jsoup.connect(url)
+                .get();
+        Elements elements = doc
+                .getElementsByClass("sc-AxjAm sc-fzowVh hQbVOc")
+                .select("a[href]");
+        String newUrl = null;
+        for (Element link : elements) {
+            newUrl = link.attr("href");
+        }
+        newUrl = origin + newUrl;
+
+        Document newDoc = Jsoup.connect(newUrl)
+                .get();
+        Elements newElements = newDoc
+                .getElementById("Data Download")
+                .select("a[href]")
+                .tagName("a");
+        String[] names = new String[]{
+                "casesReportedDaily.csv",
+                "casesReportedLatest.csv",
+                "vaccinationData.csv",
+                "vaccinationMetadata.csv"
+        };
+        int i = 0;
+        for (Element element : newElements) {
+            String eachUrl = element.attr("href");
+            downloadFile(eachUrl, names[i++]);
+        }
+    }
+
+    public static void downloadFile(String fileUrl, String fileLocal) throws Exception {
+        URL url = new URL(fileUrl);
+        HttpURLConnection urlCon = (HttpURLConnection) url.openConnection();
+//        String encValue=new String(fileUrl.getBytes(StandardCharsets.ISO_8859_1));
+        String encValue = "utf-8";
+        InputStreamReader fileReader = new InputStreamReader(urlCon.getInputStream(), encValue);
+        OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(fileLocal));
+        int len = 0;
+        while ((len = fileReader.read()) != -1) {
+            out.write((char) len);
+        }
+        out.close();
+        fileReader.close();
+    }
+
+}
